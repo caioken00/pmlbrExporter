5ecc16605c4eca03ee2dc85e10962c511252988c
caioken00 <cai0orama@gmail.com>
2017-04-24 12:52:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./REPLACE_WITH_SERIVCE_ACCOUNT.json");
+const serviceAccount = require("path/to/serviceAccountKey.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
